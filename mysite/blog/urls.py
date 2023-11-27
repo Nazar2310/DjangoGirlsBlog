a2ec9f3206bcf5e015cbc8423f67d05af6f3917a
arturhoysak <ahoysakk@gmail.com>
2023-11-27 18:00:24 +0200

    Add all files

---
+++ mysite/blog/urls.py
@@ -0,0 +1,12 @@
+from django.template.defaulttags import url
+from django.urls import path
+
+from . import views
+from .views import post_list, post_detail, post_new
+
+urlpatterns = [
+    path('', post_list),
+    url(r'^post/(?P<pk>[0-9]+)/$', post_detail, name='post_detail'),
+    url(r'^post/new/$', post_new, name='post_new'),
+]
+
